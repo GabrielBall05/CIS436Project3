9e9e0ae39f8ae11c87e5688d915fccd30e5e0ca8
Giovanni Claro <gio.claro316@gmail.com>
2026-03-29 17:45:31 -0400

    fetchBreedscode

--- project3/app/src/main/java/com/example/project3/MainActivity.kt
+++ project3/app/src/main/java/com/example/project3/MainActivity.kt
@@ -1,26 +1,40 @@
 package com.example.project3
 
+import android.app.DownloadManager
 import android.os.Bundle
+import android.util.Log
+import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.project3.databinding.ActivityMainBinding
 import com.example.project3.viewmodel.DogViewModel
 
 class MainActivity : AppCompatActivity() {
 
     //Initialize the ViewModel
     private val viewModel : DogViewModel by viewModels()
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        ViewCompat.setOnApplyWindowInsetsListener(binding.root) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        //Set button handler
+
+
+
     }
 }
--- project3/app/src/main/java/com/example/project3/viewmodel/DogViewModel.kt
+++ project3/app/src/main/java/com/example/project3/viewmodel/DogViewModel.kt
@@ -1,8 +1,16 @@
 package com.example.project3.viewmodel
 
+import android.content.Context
+import android.util.Log
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.android.volley.Request
+import com.android.volley.Response
+import com.android.volley.toolbox.JsonArrayRequest
+import com.android.volley.toolbox.Volley
+import com.example.project3.data.Constants
 import com.example.project3.data.DogBreed
+import org.json.JSONArray
 
 class DogViewModel : ViewModel() {
     //List of all the breeds as LiveData
@@ -19,18 +27,59 @@ class DogViewModel : ViewModel() {
 
     init {
         //Call fetchBreeds() right here as soon as the app has loaded to retrieve initial data from the dog api
-        fetchBreeds()
+        fetchBreeds(context: Context)
     }
 
     //To get all dog breeds upon opening app
-    fun fetchBreeds() {
+
+    fun fetchBreeds(context: Context) {
+        val baseUrl = Constants.BASE_URL
+        val apiKey = Constants.API_KEY
+        val url = "$baseUrl?api_key=$apiKey"
+
+        // 1. Create the RequestQueue
+        val queue = Volley.newRequestQueue(context)
+
+        // 2. Create the JsonArrayRequest
+        val request = object : JsonArrayRequest(
+            Request.Method.GET,
+            url,
+            null,
+            Response.Listener { response ->
+                // Success: Log the number of breeds found
+                Log.i("DogApi", "Successfully retrieved ${response.length()} breeds.")
+
+                for(i in 0 until response.length()) {
+                    val curBreed = response.getJSONObject(i)
+                    val name = curBreed.getString("name")
+                    val lifeSpan = curBreed.getString("life_span")
+                    val temperament = curBreed.getString("temperament")
+
+                    // Detailed log for each breed
+                    Log.i("DogApi", "Breed[$i]: $name | Temperament: $temperament | Life Span: $lifeSpan")
+                }
+            },
+            Response.ErrorListener  { error ->
+                // Handle error (e.g., Timeout, No Internet, 403 Unauthorized)
+                Log.i("DogApi", "Error: ${error.message}")
+            }
+        ) {} // Close object - braces are required
+
+        // 3. Add the request to the RequestQueue
+        queue.add(request)
+    } // Close fetchDogBreeds
+
+
+    //fun fetchBreeds() {
         //TODO: Use Volley to get a list of all dog breeds
         //I made a Constants.kt where there is the base url and the api key. Just do Constants.BASE_URL or .API_KEY
         //to access them. Just follow along the example he gave us for making the request and parsing the response.
         //Make sure to look at the documentation and make some requests (outside the app) to see what it gives you first.
         //TODO: Parse JSON response into objects of DogBreed class and add to a temporary MutableListOf<DogBreed>()
         //TODO: Then once the whole response is parsed, do breedsList.value = tempList
-    }
+
+        //val api = Constants.BASE_URL
+    //}
 
     //To update the selection when the spinner changes
     fun onBreedSelected(breed: DogBreed) {
